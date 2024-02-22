fd50a4f734157f660fbdc811469dca96c9d4ad83
Nihad <nihadamirov98@gmail.com>
2024-02-22 09:29:22 -0800

    update file

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,15 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
+
 		putchar(alphabet);
+	
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 		
-		for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-		{
-			putchar(alphabet);
-		}
-		putchar('\n');
-	}
+		putchar(alphabet);
+	
+
+	putchar('\n');
 
 	return (0);
 }
