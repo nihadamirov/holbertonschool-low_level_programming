38e99800d7ad833b17a3824259394ccdb7de8fb9
Nihad <nihadamirov98@gmail.com>
2024-02-22 10:50:12 -0800

    Add script prints all single digit numbers of base 10 starting from 0

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <10; num++)
-		printf("%d",num);
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 
-	printf('\n');
+	printf("\n");
 
 	return (0);
 }
