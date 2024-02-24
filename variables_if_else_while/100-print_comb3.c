009f38390291dc72a02eec5fa769932583ad604b
Nihad <nihadamirov98@gmail.com>
2024-02-23 19:28:49 -0800

    function modified

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (i != j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(',');
-				putchar(' ');
-			}
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 9 && j == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
 
 	return (0);
+}
