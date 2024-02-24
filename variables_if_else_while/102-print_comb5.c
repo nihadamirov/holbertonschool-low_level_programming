4fd15ef800efd71083d4cbef58d1510c7b5e17c2
Nihad <nihadamirov98@gmail.com>
2024-02-23 20:32:12 -0800

    function betty style

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -18,15 +18,15 @@ int main(void)
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			
+
 			if (i == 98 && j == 99)
 				continue;
-			
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
