169a1d33bdbb7f28e41b30ff524d9bb67e662d09
Nihad <nihadamirov98@gmail.com>
2024-02-23 19:50:20 -0800

    file modified

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -7,21 +7,18 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
+			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
 
-			if (i == 9 && j == 9)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
+			if (i != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 	}
 	putchar('\n');
 
