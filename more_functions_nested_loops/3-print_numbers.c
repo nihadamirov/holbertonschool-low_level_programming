204285d97ef7ef360c1d6b6e08d3dd00b573f4a8
Nihad <nihadamirov98@gmail.com>
2024-02-26 11:56:03 +0000

    Add function

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		_putchar(num);
+	}
+	_putchar(10);
+}
