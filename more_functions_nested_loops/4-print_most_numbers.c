0c7712b8c84a59775cd7e83992dfb660be12a6d7
Nihad <nihadamirov98@gmail.com>
2024-02-26 12:46:43 +0000

    Add function

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; n <= 9; num++)
+		_putchar(num + '0');
+
+	_putchar('\n');
+}
