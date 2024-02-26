f39cdca1f67e5b1518ab9b691cb54bc6eabdb303
Nihad <nihadamirov98@gmail.com>
2024-02-26 12:21:13 +0000

    modified file

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_numbers - Print the numbers since 0 up to 9 
+ * Return: The numbers since 0 up to 9
  */
+ 
 void print_numbers(void)
 {
 	int x;
