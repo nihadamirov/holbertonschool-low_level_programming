bbd67745f78f1be1db1e8fcc7953494b5b549261
Nihad <nihadamirov98@gmail.com>
2024-02-29 12:28:26 -0800

    Add function that convert a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			result = result * 10 + (*s - '0');
+		else if (result > 0)
+			break;
+		s++;
+	}
+	return result * sign;
+}
