c4a224c2b3bb60cb84757c48d82b82f467f60bf2
Nihad <nihadamirov98@gmail.com>
2024-03-01 00:29:22 -0800

    modified file

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed by a new line, to stdout.
+ * _puts - Write a function that prints a string,
+ * followed by a new line, to stdout.
  *
  * @str: This is my input String
  */
