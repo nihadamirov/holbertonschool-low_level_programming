68e95dc9a3e6f4d55071257b1d4c42b2bf9c5484
Nihad <nihadamirov98@gmail.com>
2024-03-01 01:43:07 -0800

    update file

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,8 @@ void puts2(char *str)
 
     for (i = 0; str[i] != '\0'; i+=2)
     {
-        if (str[i] >= 32 && str[i] <= 126)
+        x
+		xif (str[i] >= 32 && str[i] <= 126)
         {
             _putchar(str[i]);
         }
