a4715b5af0739656d196e516a712e87b8b239190
Nihad <nihadamirov98@gmail.com>
2024-03-02 00:00:33 -0800

    update file

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
                 dest[i] = src[i];
 
         for ( ; i < n; i++)
-                dest[i] = '\n';
+                dest[i] = '\0';
 
         return (dest);
 }
