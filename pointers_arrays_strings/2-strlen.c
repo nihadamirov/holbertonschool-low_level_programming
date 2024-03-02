3130bf428cd033b3572329819c3ed4747effe2f9
Nihad <nihadamirov98@gmail.com>
2024-03-01 23:47:42 -0800

    update file

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,25 +1,18 @@
 #include "main.h"
 
 /**
- * _strncpy - C function that copies a string, including the
- *  terminating null byte, using at most an inputted number of bytes.
- *  If the length of the source string is less than the maximum byte number,
- *  the remainder of the destination string is filled with null bytes.
- *  Works identically to the standard library function `strncpy`.
- * @dest: buffer storing the string copy
- * @src:the source string
- * @n:max nummber of byte copied
- * Return: returns
+ * _strlen - Function that returns the length of a string
+ *
+ * @s: this is the input string
+ *
+ * Return: Lenngth of the string
  */
-char *_strncpy(char *dest, char *src, int n)
+int _strlen(char *s)
 {
-	int i;
+	int index;
 
-	for (i = 0; i < n && src[i] != '\n'; i++)
-		dest[i] = src[i];
-
-	for ( ; i < n; i++)
-		dest[i] = '\n';
-
-	return (dest);
+	for (index = 0; s[index] != '\0' ; index++)
+		;
+	return (index);
 }
+
