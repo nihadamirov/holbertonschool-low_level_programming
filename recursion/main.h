6faaca56458897d4c7412f0e6982287c40c2b168
Nihad <nihadamirov98@gmail.com>
2024-03-05 13:12:37 +0000

    update file

--- recursion/main.h
+++ recursion/main.h
@@ -1,6 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char) 
+*/
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+
+#endif /*MAIN_H*/
