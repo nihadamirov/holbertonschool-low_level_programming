59d940d7d37df2d893669f5c0eb19375b139f8f2
Nihad <nihadamirov98@gmail.com>
2024-03-07 18:47:02 +0400

    Add function

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,7 +5,8 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0*/
+ * Return: 0
+ */
 int main(int argc, char __attribute__((unused)) *argv[])
 {
 printf("%d\n", argc - 1);
