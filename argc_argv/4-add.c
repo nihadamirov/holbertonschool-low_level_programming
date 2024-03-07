e2b45ee40d3e6b194d27fd739fcf77bdd3bf9939
Nihad <nihadamirov98@gmail.com>
2024-03-07 20:42:59 +0400

    betty style

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,15 +13,15 @@ int i, j, sum = 0;
 
 for (i = 1; i < argc; i++)
 {
-    for (j = 0; argv[i][j] != '\0'; j++)
-    {
-        if (argv[i][j] < '0' || argv[i][j] > '9')
-        {
-            printf("Error\n");
-            return (1);
-        }
-    }
-    sum += atoi(argv[i]);
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
 
