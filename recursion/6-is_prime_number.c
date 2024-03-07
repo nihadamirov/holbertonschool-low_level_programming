55001a2097d63036b50abc2ceaf9770c7199b03f
Nihad <nihadamirov98@gmail.com>
2024-03-07 20:53:26 +0400

    modified file

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,6 +4,9 @@
  * is_prime_number - checks if the given number is a prime number
  * @n: the number to check
  *
+ * This function checks if a given number is a prime number.
+ * It uses the is_prime function to perform the check.
+ *
  * Return: 1 if the number is prime, 0 otherwise
  */
 int is_prime_number(int n)
@@ -19,6 +22,10 @@ int is_prime_number(int n)
  * is_prime - checks if a given number is prime using recursion
  * @n: number to be checked
  * @start: number to start checking from
+ *
+ * This function checks if a given number is prime using recursion.
+ * It starts checking from the given start number and continues
+ * until it reaches 1.
  *
  * Return: 1 if n is prime, 0 otherwise
  */
