8c5605e9343fcc328523f9392ea65123758381e3
Nihad <nihadamirov98@gmail.com>
2024-03-08 11:08:54 +0400

    modified file

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,40 +1,24 @@
 #include "main.h"
 
- /**
- * is_prime_number - checks if the given number is a prime number
- * @n: the number to check
+/**
+ * is_prime_number - Write a function that returns 1 if the input integer is a
+ * prime number, otherwise return 0.
  *
- * Description: This function checks if a given number is a prime number.
- * It uses the is_prime function to perform the check.
+ * @prime: Is my number prime
  *
- * Return: 1 if the number is prime, 0 otherwise
+ * Return: 1 if the input integer is a prime number, otherwise return 0
  */
-int is_prime_number(int n)
-{
-	int start = n / 2;
 
-	if (n <= 1)
+int is_prime_number(int prime)
+{
+	if (prime <= 1)
 		return (0);
-	return (is_prime(n, start));
-}
-
- /**
- * is_prime - checks if a given number is prime using recursion
- * @n: number to be checked
- * @start: number to start checking from
- *
- * Description: This function checks if a given number is prime
- * using recursion.
- * It starts checking from the given start number and continues
- * until it reaches 1.
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime(int n, int start)
-{
-	if (start <= 1)
+	if (prime == 2)
 		return (1);
-	else if (n % start == 0)
+	if ((prime % 2 == 0) || (prime % 3 == 0) || (prime % 5 == 0))
 		return (0);
-	return (is_prime(n, start - 1));
+	else if (prime % 9 == 0 || prime % 7 == 0)
+		return (0);
+	else
+		return (1);
 }
