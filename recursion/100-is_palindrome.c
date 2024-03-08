78a51d64ae6865a2a729415b7d73b8c75306b858
Nihad <nihadamirov98@gmail.com>
2024-03-08 12:10:13 +0400

    Add function

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* is_palindrome - returns true if the given string is a palindrome
+* @s: string to check
+* Return: true if the given string is a palindrome
+*/
+int is_palindrome(char *s)
+{
+    int i; len;
+
+    for (i = 0; i < len / 2; i++)
+    {
+        if (s[i] != s[len - i - 1])
+        return (0);
+    }
+    return (1);
+}
