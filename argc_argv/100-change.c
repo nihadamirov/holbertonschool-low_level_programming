9e426999f80a58ed1cd2318c5221adfda06c234b
Nihad <nihadamirov98@gmail.com>
2024-03-08 14:29:42 +0400

    Add function

---
+++ argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that prints the minimum number of coins to make change for
+ * an amount of money.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 if successful, 1 otherwise.
+*/
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    int cents = atoi(argv[1]);
+    if (cents < 0)
+    {
+        printf("0\n");
+        return (0);
+    }
+    int coins = 0;
+    int denominations[] = {25,10,5,2,1};
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        coins += cents / denominations[i];
+        cents %= denominations[i];
+    }
+    printf("%d\n", coins);
+
+    return (0);
+}
