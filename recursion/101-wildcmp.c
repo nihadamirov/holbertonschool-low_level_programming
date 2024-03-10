9ad41d0224f58c5ce2ab3c35a1487cd2fda04603
Nihad <nihadamirov98@gmail.com>
2024-03-10 15:32:15 +0400

    modified file

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -20,7 +20,7 @@ if (s1 == s2)
 return (wildcmp(s1 + 1, s2 + 1));
 
 if(*s2 == '*')
-return ((wildcmp(s1, s2 + 1)) || (s1 == '\0' && wildcmp(s1 + 1, s2)));
+return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 
 return (0);
 }
