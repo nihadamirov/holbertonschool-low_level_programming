f49409144deb63ab68444dc3190df5c1420b5a05
Nihad <nihadamirov98@gmail.com>
2024-03-11 20:21:20 +0000

    true betty style

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,11 +17,10 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 
-	grid = malloc(height * sizeof(int*));
+	grid = malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 		return (NULL);
-	
 	
 	for (i = 0; i < height; i++)
 	{
