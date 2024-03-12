cc069bb0ad12b0a8cb663c32e4d1759bd975f007
Nihad <nihadamirov98@gmail.com>
2024-03-12 07:11:25 +0000

    Add function that concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,7 +7,7 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2)
 {
 	if (!s1) s1 = "";
 	if (!s2) s2 = "";
