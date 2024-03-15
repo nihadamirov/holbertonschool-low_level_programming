f1db23101235a9243e0ea35d5793d68a1a1b4a06
Nihad <nihadamirov98@gmail.com>
2024-03-15 23:40:18 +0400

    Add prototype

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+typedef struct dog
+{
+    name = *char;
+    age = float;
+    owner = *char;
+};
+
+#endif
