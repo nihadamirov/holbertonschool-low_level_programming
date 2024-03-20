f7e32a37225fe8fff1b52c518c6cff1f6d1c1a1a
Nihad <nihadamirov98@gmail.com>
2024-03-20 12:59:16 +0000

    modified file

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -9,18 +9,23 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div },
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{ "+", op_add },
+		{ "-", op_sub },
+		{ "*", op_mul },
+		{ "/", op_div },
+		{ "%", op_mod },
+		{ NULL, NULL }
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL)
 	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+		{
+			return (ops[i].f);
+		}
 		i++;
 	}
-	return (ops[i].f);
+	printf("Error\n");
+	exit(99);
 }
