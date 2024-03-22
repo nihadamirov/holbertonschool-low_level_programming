f51881725416cb5decbe2f7439c499217423111b
Nihad <nihadamirov98@gmail.com>
2024-03-22 16:57:40 +0000

    modified file

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (separator == NULL)
-		return;
+		printf("");
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
