66a26ccae9986c516d04590603fba01de56c58fc
Nihad <nihadamirov98@gmail.com>
2024-04-02 14:14:36 +0000

    Add function in file

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,12 +1,25 @@
 #include "main.h"
 
 /**
- *
- *
- * 
+ * print_list - function that prints all the elements of a list_t list
+ * @h: Const pointer of structure list_t
+ * Return: Number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	// TODO
+	unsigned int index = 0;
 
-}	
+	while (h != NULL)
+	{
+		if (str == NULL)
+		{
+			printf("[0] (nil)\n");
+		} else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		index++;
+	}
+	return (index);
+}
