3b47bd26fc46550229367fbf2a7d6314636e7e03
Nihad <nihadamirov98@gmail.com>
2024-04-04 14:52:59 +0000

    modifiedfile

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 /**
- * add_node - function that adds a new node
- * @head: list_t node head
- * @str: string
- * Return: the address of the new element, or NULL if it failed
+ * add_node - function that adds a new node at the beginning of a list_t list
+ * @head: Const double pointer of structure list_t for beginning
+ * @str: Const char pointer for data to be added
+ * Return: List with new node for list_t list, NULL if failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -16,9 +16,20 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
 	return (new_node);
 }
+
+unsigned int _strlen(const char *str)
+{
+    unsigned int len = 0;
+
+    while (str[len] != '\0')
+        len++;
+
+    return len;
+}
+
