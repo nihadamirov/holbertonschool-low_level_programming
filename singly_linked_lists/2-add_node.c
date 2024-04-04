cf070ff7cb42c8b5ff84957487c19cea9bca784f
Nihad <nihadamirov98@gmail.com>
2024-04-04 14:59:13 +0000

    update file

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,6 +3,21 @@
 #include <string.h>
 
 /**
+ * _strlen - function that calculates the length of a string
+ * @str: Const char pointer for the string to be measured
+ * Return: Unsigned integer value representing the length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+    unsigned int len = 0;
+
+    while (str[len] != '\0')
+        len++;
+
+    return len;
+}
+
+/**
  * add_node - function that adds a new node at the beginning of a list_t list
  * @head: Const double pointer of structure list_t for beginning
  * @str: Const char pointer for data to be added
@@ -22,14 +37,3 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (new_node);
 }
-
-unsigned int _strlen(const char *str)
-{
-    unsigned int len = 0;
-
-    while (str[len] != '\0')
-        len++;
-
-    return len;
-}
-
